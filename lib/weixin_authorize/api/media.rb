5f6ce5c58d3c025dea26dc8a58917ee055aae87f
leebo <me@bobo.im>
2016-10-06 14:45:36 +0800

    增加获取素材列表 获取素材接口

--- lib/weixin_authorize/api/media.rb
+++ lib/weixin_authorize/api/media.rb
@@ -81,68 +81,83 @@ module WeixinAuthorize
         http_post(upload_image_url, {media: file}, {type: 'image'}, 'file')
       end
 
+      # 分类型获取永久素材的列表。
+      # type 材的类型，图片（image）、视频（video）、语音 （voice）、图文（news）
+      # offset 从全部素材的该偏移位置开始返回，0表示从第一个素材 返回
+      # count 返回素材的数量，取值在1到20之间
+      def batchget_material(type, offset, count)
+        batchget_material_url = "/material/batchget_material"
+        http_post(batchget_material_url, { type: type, offset: offset, count: count })
+      end
+
+      # 根据media_id来获取永久素材
+      def get_material(media_id)
+        get_material_url = "/material/get_material"
+        http_post(get_material_url, { media_id: media_id })
+      end
+
       private
 
-        def media_base_url
-          "/media"
+      def media_base_url
+        "/media"
+      end
+
+      def process_file(media)
+        return media if media.is_a?(File) && jpep?(media)
+
+        media_url = media
+        uploader  = WeixinUploader.new
+
+        if http?(media_url) # remote
+          uploader.download!(media_url.to_s)
+        else # local
+          media_file = media.is_a?(File) ? media : File.new(media_url)
+          uploader.cache!(media_file)
         end
+        file = process_media(uploader)
+        CarrierWave.clean_cached_files! # clear last one day cache
+        file
+      end
 
-        def process_file(media)
-          return media if media.is_a?(File) && jpep?(media)
+      def process_media(uploader)
+        uploader = covert(uploader)
+        uploader.file.to_file
+      end
 
-          media_url = media
-          uploader  = WeixinUploader.new
-
-          if http?(media_url) # remote
-            uploader.download!(media_url.to_s)
-          else # local
-            media_file = media.is_a?(File) ? media : File.new(media_url)
-            uploader.cache!(media_file)
+      # JUST ONLY FOR JPG IMAGE
+      def covert(uploader)
+        # image process
+        unless (uploader.file.content_type =~ /image/).nil?
+          if !jpep?(uploader.file)
+            require "mini_magick"
+            # covert to jpeg
+            image = MiniMagick::Image.open(uploader.path)
+            image.format("jpg")
+            uploader.cache!(File.open(image.path))
+            image.destroy! # remove /tmp from MinMagick generate
           end
-          file = process_media(uploader)
-          CarrierWave.clean_cached_files! # clear last one day cache
-          file
         end
+        uploader
+      end
 
-        def process_media(uploader)
-          uploader = covert(uploader)
-          uploader.file.to_file
-        end
+      def http?(uri)
+        return false if !uri.is_a?(String)
+        uri = URI.parse(uri)
+        uri.scheme =~ /^https?$/
+      end
 
-        # JUST ONLY FOR JPG IMAGE
-        def covert(uploader)
-          # image process
-          unless (uploader.file.content_type =~ /image/).nil?
-            if !jpep?(uploader.file)
-              require "mini_magick"
-              # covert to jpeg
-              image = MiniMagick::Image.open(uploader.path)
-              image.format("jpg")
-              uploader.cache!(File.open(image.path))
-              image.destroy! # remove /tmp from MinMagick generate
-            end
-          end
-          uploader
-        end
+      def jpep?(file)
+        content_type = if file.respond_to?(:content_type)
+                         file.content_type
+                       else
+                         content_type(file.path)
+                       end
+        !(content_type =~ /jpeg/).nil?
+      end
 
-        def http?(uri)
-          return false if !uri.is_a?(String)
-          uri = URI.parse(uri)
-          uri.scheme =~ /^https?$/
-        end
-
-        def jpep?(file)
-          content_type = if file.respond_to?(:content_type)
-              file.content_type
-            else
-              content_type(file.path)
-            end
-          !(content_type =~ /jpeg/).nil?
-        end
-
-        def content_type(media_path)
-          MIME::Types.type_for(media_path).first.content_type
-        end
+      def content_type(media_path)
+        MIME::Types.type_for(media_path).first.content_type
+      end
 
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,8 +37,8 @@ ENV['CODECLIMATE_REPO_TOKEN'] = "c91fecbbd9e414e7cc3ad7a7d99207145de0ac65a3368de
 CodeClimate::TestReporter.start
 
 # If you want test, change your weixin test profile
-ENV["APPID"]="wx986f04063d341d04"
-ENV["APPSECRET"]="1a941cd88cb4579ba98ec06b6813af03"
+ENV["APPID"]="wx27b19e24f28c9275"
+ENV["APPSECRET"]="2f391a594e46820578b48a9cb2965c0c"
 ENV["OPENID"]="o9k6BuB0kydAcPTc7sPxppB1GQqA"
 ENV["TEMPLATE_ID"]="-8ooXrOK3VD3HuSS8--nH154PO9Lw2E7T-RV1uTaGLc"
 
